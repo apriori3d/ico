f9b20a67e35b1fcec6adf5bf1f94ba29ea38a6c5
Sergei Shumilin <shumilinss@gmail.com>
2026-03-03 14:26:42 +0300

    Update toy example (#5)
    Update toy example

---
+++ src/examples/toy_example.py
@@ -0,0 +1,21 @@
+from apriori.ico import IcoProcess, operator
+
+Context = tuple[int, int]
+
+
+@operator()
+def fib_step(state: Context) -> Context:
+    a, b = state
+    return (b, a + b)
+
+
+@operator()
+def first(state: Context) -> int:
+    return state[0]
+
+
+fib8 = IcoProcess(fib_step, num_iterations=8) | first
+
+print(fib8((0, 1)))  # 21
+
+fib8.describe()
